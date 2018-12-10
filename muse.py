1fc7f147339edba451cf594dbb1c768874e580ff
International <international@airmail.cc>
2018-12-10 08:39:08 -0700

    Git Initialized

---
+++ muse.py
@@ -0,0 +1,18 @@
+port = 6600
+host = 'localhost'
+instance = 'https://cybre.space'
+
+from mastodon import Mastodon
+import musicpd
+
+client = musicpd.MPDClient()
+client.connect(host, port)
+now = client.currentsong()
+client.close()
+client.disconnect()
+
+nowp = ("Now Playing: %s - %s" % (now['title'],now['artist']))
+print(nowp)
+
+mast = Mastodon(client_id='masto-muse_appcred.secret',access_token='masto-muse_usercred.secret',api_base_url=instance)
+mast.account_update_credentials(note=nowp)
